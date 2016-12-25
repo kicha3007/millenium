bba4d0865436a1ff0596bb5d43897af868af08d5
Sergey Kichigin <kicha3007@gmail.com>
2016-12-25 23:49:44 +0500

    do gradienta

--- app/js/main.js
+++ app/js/main.js
@@ -2,13 +2,13 @@ $(function() {
 	$('.vertical-slider').lightSlider({
 		gallery: true,
 		item: 1,
+		loop:true,
 		vertical: true,
 		verticalHeight: 370,
 		vThumbWidth: 50,
-		thumbItem: 4,
+		thumbItem: 5,
 		thumbMargin: 24,
 		slideMargin: 0,
-
 	});
 
 });
